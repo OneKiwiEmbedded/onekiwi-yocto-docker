2a73ed9c82497a7fc0646760cc4fee619c29a8c3
OneKiwi <productdev@ontech.com.vn>
2025-11-20 11:05:55 +0700

    add yocto-rzv2l-3.0.7-update3

---
+++ yocto-rzv2l-3.0.7-update3/get-source.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+git config --global user.email "productdev@ontech.com.vn"
+git config --global user.name  "OneKiwi"
+
+git clone https://git.yoctoproject.org/git/poky
+cd poky
+git checkout dunfell-23.0.33
+cd ../
+
+git clone https://github.com/openembedded/meta-openembedded
+cd meta-openembedded
+git checkout 01358b6d705071cc0ac5aefa7670ab235709729a
+cd ../
+
+git clone https://git.yoctoproject.org/git/meta-gplv2
+cd meta-gplv2 
+git checkout 60b251c25ba87e946a0ca4cdc8d17b1cb09292ac
+cd ../
+
+git clone https://github.com/OneKiwiEmbedded/renesas-rz-meta.git meta-renesas
+cd meta-renesas
+git checkout bsp-3.0.7-update3-dunfell/rzv2l
+cd ../
+
+git clone https://github.com/meta-qt5/meta-qt5.git
+cd meta-qt5
+git checkout -b tmp c1b0c9f546289b1592d7a895640de103723a0305
+git cherry-pick 77b6060cef9337b184100083746c2e35f531be74
+cd ../
+
+git clone https://git.yoctoproject.org/git/meta-virtualization -b dunfell
+cd meta-virtualization
+git checkout 77af3345cb99ae08b04a354b37c4afebd8b36981
+cd ../
---
+++ yocto-rzv2l-3.0.7-update3/yocto-rzv2l-3.0.7-update3.md
@@ -0,0 +1,6 @@
+# yocto-rzv2l-3.0.7-update3
+
+- `$ ./run-docker.sh rzv2l-3.0.7-update3`
+- `onekiwi@(docker)$ ./get-source.sh`
+- `onekiwi@(docker)$ ./run.sh onekiwi-rzv2l`
+- `onekiwi@(docker)$ ./run.sh`
